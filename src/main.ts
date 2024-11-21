40e709bb2aed455d278476f2b4cbb1c38da9e148
Aiden <brain1401@gmail.com>
2024-11-21 22:20:21 +0900

    chore: 포매팅 적용

--- src/main.ts
+++ src/main.ts
@@ -1,30 +1,31 @@
-import Reveal from 'reveal.js'
-import 'reveal.js/dist/reveal.css'
-import 'reveal.js/dist/theme/black.css'
-import 'highlight.js/styles/monokai.css'
-import hljs from 'highlight.js'
-import mermaid from 'mermaid'
+import Reveal from "reveal.js";
+import "reveal.js/dist/reveal.css";
+import "reveal.js/dist/theme/black.css";
+import "highlight.js/styles/monokai.css";
+import hljs from "highlight.js";
+import mermaid from "mermaid";
 
 // Reveal.js 초기화
 const deck = new Reveal({
   hash: true,
   slideNumber: true,
-  controlsBackArrows: 'faded',
+  controlsBackArrows: "faded",
   progress: true,
   keyboard: true,
   overview: true,
   center: true,
-  transition: 'slide'
-})
+  transition: "slide",
+  transitionSpeed: "default",
+});
 
 // Reveal.js 시작
-deck.initialize()
+deck.initialize();
 
 // Highlight.js 초기화
-hljs.highlightAll()
+hljs.highlightAll();
 
 // Mermaid 초기화
-mermaid.initialize({ 
+mermaid.initialize({
   startOnLoad: true,
-  theme: 'dark'
-})
+  theme: "dark",
+});
